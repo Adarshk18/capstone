d247e63f9ac8952d9f6f97bcea8c02a66feebece
Adarsh Sharma <dev.adarsh286@gmail.com>
2025-07-06 15:20:26 +0000

    new

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -10,6 +10,7 @@ contract Exchange {
     mapping(address => mapping(address=> uint256)) public tokens;
 
     event Deposit(address token, address user, uint256 amount, uint256 balance);
+    event Withdraw(address token, address user, uint256 amount, uint256 balance);
 
     constructor(address _feeAccount, uint256 _feepercent){
         feeAccount = _feeAccount;
@@ -27,6 +28,14 @@ contract Exchange {
 
         //Emit an event
         emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+    }
+
+    function withdrawToken(address _token, uint256 _amount) public{
+        Token(_token).transfer(msg.sender, _amount);
+
+        tokens[_token][msg.sender] -= _amount;
+
+        emit Withdraw(_token, msg.sender, _amount, tokens[_token][msg.sender]);
     }
 
     //check balances
